8cd11756a0be536cadcb860439801541a487086d
iflycn <iflycn@gmail.com>
2021-03-01 10:30:04 +0800

    20210301 - 修正陀螺仪问题

--- inc/jquery_sys.js
+++ inc/jquery_sys.js
@@ -21,7 +21,7 @@ $(function(){
 function Pano(v){
   v="uploadfile/pano/"+v+"/";
   embedpano({swf:"inc/pano.swf",xml:v+"pano.xml",target:"pano",html5:"prefer",passQueryParameters:false,onready:function(){
-    $("#krpanoSWFObject pre:last").html(unescape(escape($("#krpanoSWFObject pre:last").html()).replace(/%u6E56%u5357%u7701%u53CB%u8BDA%u79D1%u6280%u6709%u9650%u516C%u53F8/g,"%u5e03%u9c81%u65af%u7f51%u7edc")));
+    $("#krpanoSWFObject pre:last").html(unescape(escape($("#krpanoSWFObject pre:last").html()).replace(/Joseph%20Fouts%20Photography/g,"%u5e03%u9c81%u65af%u7f51%u7edc")));
     setTimeout(function(){
       $("#krpanoSWFObject")[0].set("plugin[button_vr].visible",true);
       $("#krpanoSWFObject")[0].set("plugin[button_rotate].visible",true);
@@ -37,7 +37,7 @@ function Pano(v){
 function Video(v){
   v="uploadfile/video/"+v+"/";
   embedpano({swf:"inc/pano.swf",xml:v+"video.xml",target:"pano",html5:"prefer",passQueryParameters:false,onready:function(){
-    $("#krpanoSWFObject pre:last").html(unescape(escape($("#krpanoSWFObject pre:last").html()).replace(/%u6E56%u5357%u7701%u53CB%u8BDA%u79D1%u6280%u6709%u9650%u516C%u53F8/g,"%u5e03%u9c81%u65af%u7f51%u7edc")));
+    $("#krpanoSWFObject pre:last").html(unescape(escape($("#krpanoSWFObject pre:last").html()).replace(/Joseph%20Fouts%20Photography/g,"%u5e03%u9c81%u65af%u7f51%u7edc")));
   }});
 }
 
